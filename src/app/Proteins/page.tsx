81594ef9341681d9de5960bc3ebf8c578cac5a2b
jacam666 <ja6cam@gmail.com>
2024-05-05 08:45:42 +0100

    fixed the depployment error

--- src/app/Proteins/page.tsx
+++ src/app/Proteins/page.tsx
@@ -6,7 +6,7 @@ export default function Protein() {
             <MyNavbar/>
             <p>
                 Protein is a must for muscle maintenance, growth, and recovery, as well as simply ensuring you get enough protein throughout the day. And whether you add it to your morning oats or whip up a satisfying shake, protein powder is a fast and easy way to up your protein intake.
-                There are loads of protein powders out there. From whey protein to vegan protein, meal replacers and more, there's a flavour and formulation for you.
+                There are loads of protein powders out there. From whey protein to vegan protein, meal replacers and more, there is a flavour and formulation for you.
             </p>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-4">
         <img src="/images/homepage-images/snc-products-3.jpg" className="w-full"/>
           <div className="absolute inset-0 flex justify-center items-center px-2 mt-9 ">
-            <span className="text-white text-center text-sm sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl border rounded-lg backdrop-brightness-50 p-4">SNC Supplement are a range of products designed to help you achieve your fitness goals, whether you want to build muscle, lose fat, improve performance or boost your immune system.</span>
+            <span className="text-white text-center sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl border rounded-lg backdrop-brightness-50 p-4">SNC Supplement are a range of products designed to help you achieve your fitness goals, whether you want to build muscle, lose fat, improve performance or boost your immune system.</span>
           </div>
         </div>
       </div>
