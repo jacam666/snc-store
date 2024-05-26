a55ba21af14576394d1b1a264bd1f44f76766b8a
jacam666 <ja6cam@gmail.com>
2024-05-26 20:16:57 +0100

    added onclick function to navbar to close after click

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,6 +11,10 @@ function MyNavbar() {
 
     const [expanded, setExpanded] = useState(false);
 
+    const handleLinkClick = () => {
+        setExpanded(false);
+    };
+
     return (
         <Navbar expand="lg" className="bg-body-tertiary" expanded={expanded}>
             <Container>
@@ -18,14 +22,14 @@ function MyNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto d-flex justify-content-center align-items-center">
-                        <Link href='/' className="text-dark mr-5" >Home</Link>
-                        <Link href='/Proteins' className="text-dark mr-5" >Proteins</Link>
-                        <Link href='/PreWorkouts' className="text-dark mr-5" >Pre-Workouts</Link>
-                        <Link href='/FatLoss' className="text-dark mr-5" >Fat Loss</Link>
-                        <Link href='/GeneralWellbeing' className="text-dark mr-5" >General Wellbeing</Link>
-                        <Link href='/TrainingPlans' className="text-dark mr-5" >Training and Diet Plans</Link>
-                        <Link href='/Studio' className="text-dark mr-5" >The Studio</Link>
-                        <Link href='/Basket' className="text-dark mr-5" >Basket</Link>
+                        <Link href='/' className="text-dark mr-5" onClick={handleLinkClick} >Home</Link>
+                        <Link href='/Proteins' className="text-dark mr-5" onClick={handleLinkClick} >Proteins</Link>
+                        <Link href='/PreWorkouts' className="text-dark mr-5" onClick={handleLinkClick} >Pre-Workouts</Link>
+                        <Link href='/FatLoss' className="text-dark mr-5" onClick={handleLinkClick} >Fat Loss</Link>
+                        <Link href='/GeneralWellbeing' className="text-dark mr-5" onClick={handleLinkClick} >General Wellbeing</Link>
+                        <Link href='/TrainingPlans' className="text-dark mr-5" onClick={handleLinkClick} >Training and Diet Plans</Link>
+                        <Link href='/Studio' className="text-dark mr-5" onClick={handleLinkClick} >The Studio</Link>
+                        <Link href='/Basket' className="text-dark mr-5" onClick={handleLinkClick} >Basket</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
