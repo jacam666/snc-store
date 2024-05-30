fa3ef497397817ff3a1adb2be3e5f35f2d9e1ac8
jacam666 <ja6cam@gmail.com>
2024-05-30 13:52:15 +0100

    changed mdbiamge to nextjs image

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -3,9 +3,10 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store';
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCol, MDBContainer, MDBIcon, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
 import { Minus, Plus, Trash2Icon } from "lucide-react";
 import { decrementQuantity, incrementQuantity, removeFromCart } from "../features/cart/cartSlice";
+import { Image } from "@nextui-org/react";
 
 const Cart: React.FC = () => {
     const cartItems = useSelector((state: RootState) => state.cart.items);
@@ -14,6 +15,7 @@ const Cart: React.FC = () => {
     const handleRemove = (itemId: number) => {
         dispatch(removeFromCart(itemId));
     };
+
     const handleDecrement = (itemId: number) => {
         dispatch(decrementQuantity(itemId));
     };
@@ -22,9 +24,7 @@ const Cart: React.FC = () => {
         dispatch(incrementQuantity(itemId));
     };
 
-    // const cartTotal = cartItems.reduce((total, item) => total + item.price, 0);
     const cartTotal = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-
 
     return (
         <section style={{ backgroundColor: "#eee" }}>
@@ -41,48 +41,53 @@ const Cart: React.FC = () => {
                                                 <p className="mb-0">You have {cartItems.length} items in your cart</p>
                                             </div>
                                         </div>
-                                        <div>
-                                            {cartItems.map((item, index) => (
-                                                <MDBCard key={index} className="mb-3">
-                                                    <MDBCardBody className="flex justify-between">
-                                                        <div className="flex">
-                                                            <MDBCardImage
-                                                                src={item.image}
-                                                                fluid
-                                                                className="rounded-3"
-                                                                style={{ width: "65px" }}
-                                                                alt="Shopping item"
-                                                            />
-                                                            <div className="ms-3">
-                                                                <MDBTypography>
-                                                                    {item.name}
-                                                                </MDBTypography>
-                                                                <p className="small mb-0">{item.size}</p>
-                                                            </div>
+                                        {cartItems.map((item) => (
+                                            <MDBCard key={item.id} className="mb-3">
+                                                <MDBCardBody className="flex justify-between">
+                                                    <div className="flex">
+                                                        {/* <MDBCardImage
+                                                            src={item.image}
+                                                            fluid
+                                                            className="rounded-3"
+                                                            style={{ width: "65px" }}
+                                                            alt="Shopping item"
+                                                        /> */}
+                                                        <Image
+                                                            src={item.image}
+                                                            width={65}
+                                                            height={65}
+                                                            className="rounded-3"
+                                                            alt="Shopping item"
+                                                        />
+                                                        <div className="ms-3">
+                                                            <MDBTypography>
+                                                                {item.name}
+                                                            </MDBTypography>
+                                                            <p className="small mb-0">{item.size}</p>
                                                         </div>
-                                                        <div className="flex ml-2">
-                                                            <div>
-                                                                <button className="btn btn-primary btn-sm me-2" onClick={() => handleDecrement(item.id)}>
-                                                                    <Minus size={16} />
-                                                                </button>
-                                                                <span>{item.quantity}</span>
-                                                                <button className="btn btn-primary btn-sm ms-2" onClick={() => handleIncrement(item.id)}>
-                                                                    <Plus size={16} />
-                                                                </button>
-                                                            </div>
-                                                            <div style={{ width: "80px" }} className="ml-2">
-                                                                <MDBTypography tag="h5" className="mb-0">
-                                                                    £{item.price * item.quantity}
-                                                                </MDBTypography>
-                                                            </div>
-                                                            <a href="#!" onClick={() => handleRemove(item.id)}>
-                                                                <Trash2Icon />
-                                                            </a>
+                                                    </div>
+                                                    <div className="flex ml-2">
+                                                        <div>
+                                                            <button className="btn btn-primary btn-sm me-2" onClick={() => handleDecrement(item.id)}>
+                                                                <Minus size={16} />
+                                                            </button>
+                                                            <span>{item.quantity}</span>
+                                                            <button className="btn btn-primary btn-sm ms-2" onClick={() => handleIncrement(item.id)}>
+                                                                <Plus size={16} />
+                                                            </button>
                                                         </div>
-                                                    </MDBCardBody>
-                                                </MDBCard>
-                                            ))}
-                                        </div>
+                                                        <div style={{ width: "80px" }} className="ml-2">
+                                                            <MDBTypography tag="h5" className="mb-0">
+                                                                £{(item.price * item.quantity).toFixed(2)}
+                                                            </MDBTypography>
+                                                        </div>
+                                                        <a href="#!" onClick={() => handleRemove(item.id)}>
+                                                            <Trash2Icon />
+                                                        </a>
+                                                    </div>
+                                                </MDBCardBody>
+                                            </MDBCard>
+                                        ))}
                                         <MDBCard className="text-center text-3xl mt-4">
                                             <p>Total = £{cartTotal.toFixed(2)}</p>
                                         </MDBCard>
